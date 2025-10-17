836b5abd65fc3d7cabcd340feb44f448a4c16e78
laurabas05 <laaurabas@gmail.com>
2025-10-17 16:43:19 +0200

    Segunda modificación del archivo js/gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,4 +1,6 @@
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(nuevoPresupuesto) {
     if (typeof nuevoPresupuesto === "number" && nuevoPresupuesto >= 0) {
@@ -14,13 +16,24 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     if (typeof valor !== "number" || valor < 0) {
         valor = 0;
     }
 
     this.descripcion = descripcion;
     this.valor = valor;
+    
+    if (!fecha) {
+        this.fecha = Date.now();
+    } else {
+        let fechaParse = Date.parse(fecha);
+        this.fecha = isNaN(fechaParse) ? Date.now() : fechaParse;
+        // si no se introduce una fecha válida, se establece la fecha actual
+    }
+
+    this.etiquetas = etiquetas.length > 0 ? etiquetas : [];
+    // si no se introducen etiquetas, se establece un array vacío
 
     this.mostrarGasto = function() {
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
@@ -35,10 +48,60 @@ function CrearGasto(descripcion, valor) {
             this.valor = nuevoValor;
         }
     };
+
+    this.mostrarGastoCompleto = function() {
+        let textoSalida = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.`;
+        textoSalida += `\nFecha: ${new Date(this.fecha).toLocaleString()}`;
+        textoSalida += `\nEtiquetas:\n`;
+        this.etiquetas.forEach(etiqueta => textoSalida += `- ${etiqueta}\n`);
+        return textoSalida;
+    };
+
+    this.actualizarFecha = function(nuevaFecha) {
+        let fechaParse = Date.parse(nuevaFecha);
+        if (!isNaN(fechaParse)) this.fecha = fechaParse;
+    };
+
+    this.anyadirEtiquetas = function(...nuevasEtiquetas) {
+        nuevasEtiquetas.forEach(etiqueta => {
+            if (!this.etiquetas.includes(etiqueta)) this.etiquetas.push(etiqueta);
+        });
+    };
+
+    this.borrarEtiquetas = function(...etiquetas) {
+        this.etiquetas = this.etiquetas.filter(etiqueta => !etiquetas.includes(etiqueta));
+    };
+}
+
+function listarGastos() {
+    return gastos;
+}
+
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    gastos.push(gasto);
+    idGasto++;
+}
+
+function borrarGasto(id) {
+    gastos = gastos.filter(gasto => gasto.id !== id);
+}
+
+function calcularTotalGastos() {
+    return gastos.reduce((total, gasto) => total + gasto.valor, 0);
+}
+
+function calcularBalance() {
+    return presupuesto - calcularTotalGastos();
 }
 
 export {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
 }
