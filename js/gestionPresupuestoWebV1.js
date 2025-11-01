bec996be65dff7a6da1065e92d4216a8dd47c231
laurabas05 <laaurabas@gmail.com>
2025-11-01 16:49:46 +0100

    Creado gestionPresupuestoWebV1.js. Importado el anterior js, y en la función crearFormularioGasto he creado los campos del formulario y los he añadido al formulario

---
+++ js/gestionPresupuestoWebV1.js
@@ -0,0 +1,52 @@
+import {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
+} from 'gestionPresupuesto.js';
+
+function crearFormularioGasto() {
+    const contenedor = document.getElementById("formularioGastos");
+
+    const form = document.createElement("form");
+
+    const inputDescripcion = document.createElement("input");
+    inputDescripcion.type = "text";
+    inputDescripcion.id = "descripcion";
+    inputDescripcion.placeholder = "Descripción del gasto";
+    inputDescripcion.required = "true";
+
+    const inputValor = document.createElement("input");
+    inputValor.type = "number";
+    inputValor.id = "valor";
+    inputValor.placeholder = "Importe del gasto (€)";
+    inputValor.required = true;
+
+    const inputFecha = document.createElement("input");
+    inputFecha.type = "date";
+    inputFecha.id = "fecha";
+    inputFecha.placeholder = "Fecha del gasto";
+
+    const inputEtiquetas = document.createElement("input");
+    inputEtiquetas.type = "text";
+    inputEtiquetas.id = "etiquetas";
+    inputEtiquetas.placeholder = "Etiquetas del gasto";
+
+    const botonAniadirGasto = document.createElement("button");
+    botonAniadirGasto.type = "submit";
+    botonAniadirGasto.textContent = "Añadir gasto";
+
+    form.append(
+        inputDescripcion,
+        inputValor,
+        inputFecha,
+        inputEtiquetas,
+        botonAniadirGasto
+    );
+}
