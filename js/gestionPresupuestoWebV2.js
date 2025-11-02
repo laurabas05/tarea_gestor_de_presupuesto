3a126a0af4aa56b2ca21a36dbfa62be52f40d455
laurabas05 <laaurabas@gmail.com>
2025-11-02 01:02:23 +0100

    Creado gestionPresupuestoWebV2.js e importado el archivo gestionPresupuesto.js

---
+++ js/gestionPresupuestoWebV2.js
@@ -0,0 +1,12 @@
+import {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
+} from './gestionPresupuesto.js';
